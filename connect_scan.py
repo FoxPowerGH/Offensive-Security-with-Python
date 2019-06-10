2dc8dc8d8b1347f0955774448719f26ec27648d0
Jorge Rodriguez <foxpower@gmail.com>
2019-06-09 21:32:56 -0500

    print (port+": Puerto Cerrado")

--- connect_scan.py
+++ connect_scan.py
@@ -21,4 +21,11 @@ scanPorts = ports.strip().split(':')
 for port in scanPorts:
     response = sr1(IP(dst = dst_ip) / TCP(sport = src_port, dport = int(port), flags="S"))
     if (str(type(response))) == "<type 'NoeType'>":
+        print (port+": Puerto Cerrado")
+    elif (response.haslayer(TCP)):
+        if (response.getlayer(TCP).flags == 0x12):
+            send_rst = sr1(IP(dst = dst_ip) / TCP (sport = src_port, dport = int(port), flags="AR"))
+            print (port+": Puerto Abierto")
+    elif (response.getlayer(TCP).flags == 0x14):
+        print (port+": Puerto Cerrado")
 
